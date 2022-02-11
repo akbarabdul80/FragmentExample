2a0d2f32af2cd889301d5ed706077f93236a6b59
zerodev <akbarabdul80@gmail.com>
2022-02-11 16:51:17 +0700

    add fragment_communication

--- app/src/main/java/com/example/fragmentexample1updated/MainActivity.java
+++ app/src/main/java/com/example/fragmentexample1updated/MainActivity.java
@@ -3,14 +3,19 @@ package com.example.fragmentexample1updated;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SimpleFragment.OnFragmentInteractionListener {
     private Button openButton;
     private Boolean isFragmentDisplayed = false;
+
+    private final String FRAGMENT_STATE = "fragment-state";
+    private int mCurrentChoice = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-        SimpleFragment simpleFragment = SimpleFragment.newInstance();
+        SimpleFragment simpleFragment = SimpleFragment.newInstance(mCurrentChoice);
         fragmentTransaction.add(R.id.fragment_container, simpleFragment).addToBackStack(null).commit();
 
         isFragmentDisplayed = true;
@@ -47,5 +52,17 @@ public class MainActivity extends AppCompatActivity {
 
         isFragmentDisplayed = false;
         openButton.setText(R.string.open);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(FRAGMENT_STATE, isFragmentDisplayed);
+    }
+
+    @Override
+    public void onRadioButtonChoiceChecked(int choice) {
+        mCurrentChoice = choice;
+        Toast.makeText(this, "choice is " + choice, Toast.LENGTH_SHORT).show();
     }
 }
