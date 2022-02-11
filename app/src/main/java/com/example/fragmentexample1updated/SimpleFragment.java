4d35d2604a76ff4c920283f672629ee9721804ed
zerodev <akbarabdul80@gmail.com>
2022-02-11 16:41:37 +0700

    add dynamic text articleQuestionTextView

--- app/src/main/java/com/example/fragmentexample1updated/SimpleFragment.java
+++ app/src/main/java/com/example/fragmentexample1updated/SimpleFragment.java
@@ -4,10 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 public class SimpleFragment extends Fragment {
+
+    private static final int YES = 0;
+    private static final int NO = 1;
 
     public SimpleFragment() {
     }
@@ -25,7 +31,28 @@ public class SimpleFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_simple, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_simple, container, false);
+
+        RadioGroup radioGroup = view.findViewById(R.id.radio_group);
+        TextView articleQuestionTextView = view.findViewById(R.id.question_textview);
+
+        radioGroup.setOnCheckedChangeListener((radioGroup1, i) -> {
+            RadioButton btn = radioGroup1.findViewById(i);
+            int selectedIndex = radioGroup1.indexOfChild(btn);
+
+            switch (selectedIndex) {
+                case YES:
+                    articleQuestionTextView.setText(R.string.yes_message);
+                    break;
+                case NO:
+                    articleQuestionTextView.setText(R.string.no_message);
+                    break;
+                default:
+                    break;
+            }
+        });
+
+        return view;
     }
 }
